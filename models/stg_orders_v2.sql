18fde59e0a43558a0a26a351ebcf9392b29704aa
NADJITIESSEM GONDJE Victoire <victoiren79@gmail.com>
2026-08-02 12:27:49 -0400

    Auto-fix: user_id -> customer_id in models/stg_orders_v2.sql

--- models/stg_orders_v2.sql
+++ models/stg_orders_v2.sql
@@ -1,7 +1,7 @@
 -- stg_orders_v2.sql
--- Staging model: simple passthrough, references raw_orders_v2.user_id directly.
+-- Staging model: simple passthrough, references raw_orders_v2.customer_id as user_id directly.
 select
     user_id,
-    order_number as order_id,
+    order_id,
     order_total
 from raw_orders_v2
