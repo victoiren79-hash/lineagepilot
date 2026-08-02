b3c3e88776c150c43f639d3acc0757eb56f8ecc6
NADJITIESSEM GONDJE Victoire <victoiren79@gmail.com>
2026-08-02 12:22:34 -0400

    Auto-fix: order_id -> order_number in models/fct_revenue_v2.sql (#5)

--- models/fct_revenue_v2.sql
+++ models/fct_revenue_v2.sql
@@ -1,8 +1,8 @@
 -- fct_revenue_v2.sql
--- Revenue fact: JOINS on raw_orders_v2.user_id and aggregates order_sum.
+-- Revenue fact: JOINS on raw_orders_v2.user_id and aggregates order_total.
 select
     o.user_id,
-    sum(o.order_sum) as total_revenue
+    sum(o.order_total) as total_revenue
 from raw_orders_v2 o
 join customers c
     on o.user_id = c.user_id
